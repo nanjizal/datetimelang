9509c9ca0c9a6eb561dffdb36accd0028a77aa2b
nanjizal <nanjizal@users.noreply.github.com>
2021-04-16 13:12:11 +0100

    save spanish days of week.

---
+++ src/datetimelang/spanish/DayName.hx
@@ -0,0 +1,93 @@
+package datetimelang.spanish;
+/*
+lunes	martes	miércoles	jueves	viernes	sábado	domingo
+*/
+import datetime.utils.DateTimeUtils;
+import datetime.DateTime;
+import datetime.DateTimeInterval;
+
+abstract DayName( DTWeekDay ) to DTWeekDay from DTWeekDay {
+    public inline
+    function new( d: DTWeekDay ){
+        this = d;
+    }
+    @:from
+    static inline public function fromString( s: String ): Null<DayName> {
+        return new DayName( switch( s.toLowerCase() ){
+            case 'lunes':
+                return Monday;
+            case 'martes':
+                return Tuesday;
+            case 'miércoles':
+                return Wednesday;
+            case 'miercoles':
+                return Wednesday;
+            case 'jueves':
+                return Thursday;
+            case 'viernes':
+                return Friday;
+            case 'sábado':
+                return Saturday;
+            case 'sabado':
+                return Saturday;
+            case 'domingo':
+                return Sunday;
+            case _:
+                return null;
+        } );
+    }
+    public inline function name( len: Int = -1 ){
+        var str: String = toString();
+        return if( len == -1 ){
+            str;
+        } else {
+            str.substr( 0, len );
+        }
+    }
+    public inline function lowerCase( len: Int = -1 ){
+        return name( len ).toLowerCase();
+    }
+    public inline function allUpperCase( len: Int = -1 ){
+        return name( len ).toUpperCase();
+    }
+    @:to
+    public inline function toString() {
+        var d: DTWeekDay = cast this;
+        return switch( d ){
+            case Monday:
+                return 'Lunes';
+            case Tuesday:
+                return 'Martes';
+            case Wednesday:
+                return 'Mieroles';
+            case Thursday:
+                return 'Jueves';
+            case Friday:
+                return 'Viernes';
+            case Saturday:
+                return 'Sábado';
+            case Sunday:
+                return 'Domingo';
+        }
+    }
+    public static inline function stringAllUpFromDateTime( dt: DateTime, len: Int = -1, mondayBased:Bool = false ){
+        var m: MonthName = cast( dt.getWeekDay(mondayBased), DTWeekDay );
+        var s: String = m.upperCase( len );
+        return s;
+    }           
+    public static inline function stringLowerFromDateTime( dt: DateTime, len: Int = -1, mondayBased:Bool = false ){
+        var m: MonthName = cast( dt.getWeekDay(mondayBased), DTWeekDay );
+        var s: String = m.lowerCase( len );
+        return s;
+    }        
+    public static inline function stringShortFromDateTime( dt: DateTime, len: Int = -1, mondayBased:Bool = false ){
+        var m: MonthName = cast( dt.getWeekDay(mondayBased), DTWeekDay );
+        var s: String = m.name( len );
+        return s;
+    }        
+    public static inline function stringFromDateTime( dt: DateTime, mondayBased:Bool = false ): String {
+        var m: WeekDayLongName = cast( dt.getWeekDay(mondayBased), DTWeekDay );
+        var s: String = m;
+        return s;
+    }
+}
