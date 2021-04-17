4a1c675d5d60f13ed009f099582ea2612c43a369
nanjizal <nanjizal@users.noreply.github.com>
2021-04-17 01:48:21 +0100

    Create Doc.hx

---
+++ src/datetimelang/upkeep/Doc.hx
@@ -0,0 +1,12 @@
+package datetimelang.upkeep;
+import datetimelang.*;
+
+@:dox(hide)
+class Doc {
+    public static function main(){
+        
+    }
+    public function new(){
+        
+    }
+}
